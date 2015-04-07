334e4834916d339c7f08e712f337d86b5e4f3d0a
Nick Van Wiggeren <nvanwiggeren@digitalocean.com>
2015-04-07 15:42:43 -0400

    cbzip2: handle writes, flushes and closes properly

--- writer.go
+++ writer.go
@@ -30,7 +30,7 @@ func NewWriter(w io.Writer) (*Writer, error) {
 	wrtr.bz = &C.bz_stream{bzalloc: nil, bzfree: nil, opaque: nil}
 
 	if result := C.BZ2_bzCompressInit(wrtr.bz, blockSize, verbosity, workFactor); result != BZ_OK {
-		return nil, ErrInit
+		return nil, retCodeToErr(int(result))
 	}
 
 	return wrtr, nil
@@ -42,33 +42,6 @@ func (b *Writer) Write(d []byte) (int, error) {
 	if b.err != nil {
 		return 0, b.err
 	}
-	return b.write(d, BZ_RUN)
-}
-
-// Flush writes any pending data to the underlying writer.
-func (b *Writer) Flush() error {
-	if b.err != nil {
-		return b.err
-	}
-	_, err := b.write(nil, BZ_FLUSH)
-	return err
-}
-
-// Close closes the writer, flushing any unwritten data to the underlying io.Writer
-// Close does not close the underlying io.Writer.
-func (b *Writer) Close() error {
-	if b.err != nil {
-		return b.err
-	}
-	if _, err := b.write(nil, BZ_FINISH); err != nil {
-		return err
-	}
-	C.BZ2_bzCompressEnd(b.bz)
-	b.err = io.EOF
-	return nil
-}
-
-func (b *Writer) write(d []byte, flush int) (int, error) {
 	if len(d) == 0 {
 		b.bz.avail_in = 0
 		b.bz.next_in = (*C.char)(unsafe.Pointer(nil))
@@ -77,29 +50,81 @@ func (b *Writer) write(d []byte, flush int) (int, error) {
 		b.bz.next_in = (*C.char)(unsafe.Pointer(&d[0]))
 	}
 
-	// loop until we don't have a full output buffer
-	// this will also write to the underlying writer
+	// loop until there's no more input data
 	for {
-		// give the compressor our output buffer
-		b.bz.next_out = (*C.char)(unsafe.Pointer(&b.out[0]))
-		b.bz.avail_out = (C.uint)(len(b.out))
-		// add data with our specified call to the buffer
-		if ret := C.BZ2_bzCompress(b.bz, (C.int)(flush)); ret < 0 {
-			b.err = ErrBadCompression
+		_, err := b.compress(BZ_RUN)
+		if err != nil {
+			b.err = err
 			return 0, b.err
 		}
-		// we have (total length) - (space available) of data
-		have := len(b.out) - int(b.bz.avail_out)
-		_, b.err = b.w.Write(b.out[:have])
-		if b.err != nil {
-			C.BZ2_bzCompressEnd(b.bz)
-			return 0, b.err
-		}
-		// we have available output buffer, drop out
-		// and get more data
-		if b.bz.avail_out != 0 {
+		// if we've processed all of the input, break
+		if b.bz.avail_in == 0 {
 			break
 		}
 	}
 	return len(d), nil
+}
+
+// Flush writes any pending data to the underlying writer.
+func (b *Writer) Flush() error {
+	if b.err != nil {
+		return b.err
+	}
+	for {
+		ret, err := b.compress(BZ_FLUSH)
+		if err != nil {
+			b.err = err
+			return b.err
+		}
+		// if we're done flushing, return
+		if ret == BZ_RUN_OK {
+			break
+		}
+	}
+	return nil
+}
+
+// Close closes the writer, flushing any unwritten data to the underlying io.Writer
+// Close does not close the underlying io.Writer.
+func (b *Writer) Close() error {
+	if b.err != nil {
+		return b.err
+	}
+	for {
+		ret, err := b.compress(BZ_FINISH)
+		if err != nil {
+			b.err = err
+			return b.err
+		}
+		// When we get to the actual end of the stream, break
+		if ret == BZ_STREAM_END {
+			break
+		}
+	}
+
+	C.BZ2_bzCompressEnd(b.bz)
+	b.err = io.EOF
+	return nil
+}
+
+func (b *Writer) compress(flush int) (int, error) {
+	// give the compressor our output buffer
+	b.bz.next_out = (*C.char)(unsafe.Pointer(&b.out[0]))
+	b.bz.avail_out = (C.uint)(len(b.out))
+
+	// add data with our specified call to the buffer
+	ret := C.BZ2_bzCompress(b.bz, (C.int)(flush))
+	if ret < 0 {
+		return 0, retCodeToErr(int(ret))
+	}
+
+	// we have (total length) - (space available) of data
+	have := len(b.out) - int(b.bz.avail_out)
+	_, err := b.w.Write(b.out[:have])
+	if err != nil {
+		C.BZ2_bzCompressEnd(b.bz)
+		return 0, err
+	}
+
+	return int(ret), nil
 }
